e078ad3f85a71cab8ce4d579d3c5b4dee11fdf49
hafizfaizan97 <165157377+hafizfaizan97@users.noreply.github.com>
2024-10-04 16:09:44 -0700

    # order table set

--- database/migrations/2022_12_10_134924_create_order_table.php
+++ database/migrations/2022_12_10_134924_create_order_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('order', function (Blueprint $table) {
-           
+
             $table->id();
             $table->string('user_id');
             $table->string('lname');
@@ -30,7 +30,6 @@ return new class extends Migration
             $table->tinyInteger('status')->default('0');
             $table->string('message')->nullable();
             $table->string('tracking_no');
-            $table->string('total_price');
             $table->timestamps();
         });
     }
